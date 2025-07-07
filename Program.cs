a907059beffe70b8ee7ef44c27d2f318848e860e
Bernardo Werner <bernardo.wr@gmail.com>
2025-07-07 09:27:49 -0300

    feat: :sparkles: Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone1 = new Iphone("15", "Pro", "98745654", 256);
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia1 = new Nokia("Pé de boi", "Velho", "9884854", 16);
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Youtube");
+
+
+
+
